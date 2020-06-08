448d44812a63e502cc4fcba639e4cc5e8e7cc749
igor <shevchenkcoigor@gmail.com>
2020-06-08 08:33:18 +0300

    add hash_str_uint32

--- Application/src/network/modulesgroupsfiller.cpp
+++ Application/src/network/modulesgroupsfiller.cpp
@@ -1,16 +1,38 @@
 #include "modulesgroupsfiller.h"
 #include "modulesgroups.h"
+#include "modules.h"
+#include <set>
 
 ModulesGroupsFiller::ModulesGroupsFiller()
 {
     connect(&registry, &Registry::decompressSuccess, this, &ModulesGroupsFiller::makeTransform);
 }
 
+inline uint32_t hash_str_uint32(const std::string& str) {
+
+    uint32_t hash = 0x811c9dc5;
+    uint32_t prime = 0x1000193;
+
+    for(int i = 0; i < str.size(); ++i) {
+        uint8_t value = str[i];
+        hash = hash ^ value;
+        hash *= prime;
+    }
+
+    return hash;
+
+}
+
 void ModulesGroupsFiller::makeTransform(const QJsonArray& source)
 {
-    std::vector<ModulesGroups> target;
-    std::transform(source.begin(), source.end(), std::back_inserter(target),
-                   [](const QJsonValue& value) {
-        return ModulesGroups(value.toObject());
-    });
+    std::set<uint32_t> set;
+    std::vector<ModulesGroups> groups;
+    std::vector<Modules> modules;
+
+    for (QJsonArray const_iterator = source.begin(); )
+//    std::vector<ModulesGroups> target;
+//    std::transform(source.begin(), source.end(), std::back_inserter(target),
+//                   [](const QJsonValue& value) {
+//        return ModulesGroups(value.toObject());
+//    });
 }
