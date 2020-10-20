ba0928e275883f950038a5fd558b9c29c44ffc47
igor <shevchenkcoigor@gmail.com>
2020-10-20 10:27:46 +0300

    sqlite_orm 1.6; need fix

--- 3rdparty/3rdparty.cmake
+++ 3rdparty/3rdparty.cmake
@@ -4,20 +4,20 @@ if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
 # Update submodules as needed
     option(GIT_SUBMODULE "Check submodules during build" ON)
     if(GIT_SUBMODULE)
-        message(STATUS "Submodule update")
-        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                        RESULT_VARIABLE GIT_SUBMOD_RESULT)
-        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
-        else()
-            set(QUAZIP_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/quazip/quazip
-                    CACHE PATH "quazip include directory")
-            set(SQLITE_ORM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/sqlite_orm/include/sqlite_orm
-                    CACHE PATH "sqlite_orm include directory")
-            add_subdirectory(3rdparty/quazip)
-            add_subdirectory(3rdparty/sqlite_orm)
-        endif()
+#        message(STATUS "Submodule update")
+#        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+#                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+#                        RESULT_VARIABLE GIT_SUBMOD_RESULT)
+#        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+#            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+#        else()
+        set(QUAZIP_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/quazip/quazip
+                CACHE PATH "quazip include directory")
+        set(SQLITE_ORM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/sqlite_orm/include/sqlite_orm
+                CACHE PATH "sqlite_orm include directory")
+        add_subdirectory(3rdparty/quazip)
+        add_subdirectory(3rdparty/sqlite_orm)
+#        endif()
     endif()
 endif()
 
