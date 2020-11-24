f6d0a0fd0229f5acce4b05ad7c716fc858dbbe43
igor <shevchenkcoigor@gmail.com>
2020-11-24 17:50:05 +0200

    set name branch in .gitmodules; new way git checkout submodules

--- 3rdparty/3rdparty.cmake
+++ 3rdparty/3rdparty.cmake
@@ -3,32 +3,26 @@ find_package(Git QUIET)
 option(BUILD_EXAMPLES OFF) # option BUILD_EXAMPLES available sinse with sqlite_orm==1.6
 option(SqliteOrm_BuildTests OFF)
 
-execute_process(COMMAND ${GIT_EXECUTABLE} submodule
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    RESULT_VARIABLE GIT_SUBMODULE)
-message(${CMAKE_CURRENT_SOURCE_DIR})
-message(${PROJECT_SOURCE_DIR})
-execute_process(COMMAND ls -la)
+if(EXISTS "${PROJECT_SOURCE_DIR}/.git")
+    message(STATUS "========================Submodule update========================")
+    execute_process(COMMAND bash -c "git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'"
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                    RESULT_VARIABLE GIT_SUBMOD_RESULT)
+    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+        message(FATAL_ERROR "git submodule update --init failed with : ${GIT_SUBMOD_RESULT}, please checkout submodules")
+    else()
+        set(SQLITE_ORM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/sqlite_orm/include/sqlite_orm
+                CACHE PATH "sqlite_orm include directory")
 
-if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
-# Update submodules as needed
-    option(GIT_SUBMODULE "Check submodules during build" ON)
-    if(GIT_SUBMODULE)
-        message(STATUS "Submodule update")
-        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                        RESULT_VARIABLE GIT_SUBMOD_RESULT)
-        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-            message(FATAL_ERROR "git submodule update --init failed with : ${GIT_SUBMOD_RESULT}, please checkout submodules")
-        else()
+        if (NOT ${SAILFISH})
             set(QUAZIP_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/quazip/quazip
                     CACHE PATH "quazip include directory")
-            set(SQLITE_ORM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/sqlite_orm/include/sqlite_orm
-                    CACHE PATH "sqlite_orm include directory")
             add_subdirectory(3rdparty/quazip)
-            add_subdirectory(3rdparty/sqlite_orm)
             add_subdirectory(3rdparty/openssl)
         endif()
+        execute_process(COMMAND bash -c "sed -e '/add_subdirectory(examples)/ s/^#*/#/' -i 3rdparty/sqlite_orm/CMakeLists.txt"
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+        add_subdirectory(3rdparty/sqlite_orm)
     endif()
 endif()
 
