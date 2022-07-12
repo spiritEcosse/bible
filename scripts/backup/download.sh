59cdf05bab42c9c2afd13974505de1b126d1a602
spiritEcosse <shevchenkcoigor@gmail.com>
2022-07-12 23:35:06 +0200

    BCPA-66 Github actions - broken cache

--- scripts/backup/download.sh
+++ scripts/backup/download.sh
@@ -2,9 +2,8 @@
 
 echo "${IDENTITY_FILE}" > "${ID_FILE}" &&
 chmod 600 "${ID_FILE}" &&
-echo "$PWD"
-
-scp -i "${ID_FILE}" "${BACKUP_SERVER}":~/backups/"${FILE}" .
+ssh-keyscan -H "${BACKUP_HOST}" >> ~/.ssh/known_hosts &&
+scp -i "${ID_FILE}" "${BACKUP_USER}"@"${BACKUP_HOST}":~/backups/"${FILE}" .
 
 RESULT_SCP=$?
 
--- scripts/backup/upload.sh
+++ scripts/backup/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tar --exclude="${FILE}" -zcf "${FILE}" . &&
-scp -i "${ID_FILE}" "${FILE}" "${BACKUP_SERVER}":~/backups/
+scp -i "${ID_FILE}" "${FILE}" "${BACKUP_USER}"@"${BACKUP_HOST}":~/backups/
