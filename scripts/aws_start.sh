ac5e115b36dc40dabfaf2db9d0b0a67906642b4d
spiritEcosse <shevchenkcoigor@gmail.com>
2022-08-13 17:29:48 +0200

    BCPA-71 Wakeup AWS backup server on demand

--- scripts/aws_start.sh
+++ scripts/aws_start.sh
@@ -4,5 +4,6 @@ sudo zypper -n install python3-pip &&
 sudo pip install awscli &&
 aws --version &&
 echo "-------------------------------- Start instance --------------------------------- " &&
+export AWS_REGION=us-east-1 &&
 aws ec2 start-instances --instance-ids "${EC2_INSTANCE}" &&
 sleep 20
