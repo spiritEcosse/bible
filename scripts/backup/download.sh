98838d1e64c210fddb86442d6de6640263e160fb
spiritEcosse <shevchenkcoigor@gmail.com>
2022-08-13 11:00:15 +0200

    BCPA-71 Wakeup AWS backup server on demand

--- scripts/backup/download.sh
+++ scripts/backup/download.sh
@@ -4,7 +4,8 @@ echo "${IDENTITY_FILE}" > "${ID_FILE}" &&
 chmod 600 "${ID_FILE}" &&
 mkdir -p ~/.ssh/ &&
 touch ~/.ssh/known_hosts &&
-export BACKUP_HOST=$(aws ec2 describe-instances --instance-ids "${EC2_INSTANCE}" --query "Reservations[*].Instances[*].[PublicIpAddress]" --output text)
+aws ec2 describe-instances --instance-ids "${EC2_INSTANCE}" --query "Reservations[*].Instances[*].[PublicIpAddress]" --output text
+BACKUP_HOST=$?
 echo "BACKUP_HOST: ${BACKUP_HOST}"
 echo "EC2_INSTANCE: ${EC2_INSTANCE}"
 ssh-keyscan -H "${BACKUP_HOST}" >> ~/.ssh/known_hosts &&
