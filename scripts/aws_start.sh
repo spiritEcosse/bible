42f8c4e66efe54006b8e3b313f307c403d8bdcdf
spiritEcosse <shevchenkcoigor@gmail.com>
2022-08-14 10:14:22 +0200

    BCPA-71 Wakeup AWS backup server on demand

--- scripts/aws_start.sh
+++ scripts/aws_start.sh
@@ -8,5 +8,4 @@ mkdir ~/.aws/ &&
 echo "[default]
 region = ${AWS_REGION}" > ~/.aws/config &&
 aws ec2 start-instances --instance-ids "${EC2_INSTANCE}" &&
-sleep 20 &&
-export BACKUP_HOST=$(aws ec2 describe-instances --instance-ids "${EC2_INSTANCE}" --query "Reservations[*].Instances[*].[PublicIpAddress]" --output text)
+sleep 20
