50bff2309afc315094248c77d9b8628138e2866b
spiritEcosse <shevchenkcoigor@gmail.com>
2022-08-14 10:09:33 +0200

    BCPA-71 Wakeup AWS backup server on demand

--- scripts/backup/download.sh
+++ scripts/backup/download.sh
@@ -5,7 +5,8 @@ chmod 600 "${ID_FILE}" &&
 mkdir -p ~/.ssh/ &&
 touch ~/.ssh/known_hosts &&
 ssh-keyscan -H "${BACKUP_HOST}" >> ~/.ssh/known_hosts &&
-scp -i "${ID_FILE}" "${BACKUP_USER}"@"${BACKUP_HOST}":~/backups/"${FILE}" . &&
+scp -i "${ID_FILE}" "${BACKUP_USER}"@"${BACKUP_HOST}":~/backups/"${FILE}" .
+
 RESULT_SCP=$?
 
 if [ $RESULT_SCP -eq 0 ]; then
