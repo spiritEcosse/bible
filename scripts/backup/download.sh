4de08626f2efd34c707bddb76baacdfa9cf52fb2
spiritEcosse <shevchenkcoigor@gmail.com>
2022-08-26 17:26:48 +0200

    BCPA-72-clion

--- scripts/backup/download.sh
+++ scripts/backup/download.sh
@@ -2,19 +2,11 @@
 
 source build/scripts/header.sh
 
-
+echo '================================ Start download backup ===================================' &&
 echo "${IDENTITY_FILE}" > "${ID_FILE}" &&
 chmod 600 "${ID_FILE}" &&
 mkdir -p ~/.ssh/ &&
 touch ~/.ssh/known_hosts &&
 prepare_aws_instance &&
-scp -i "${ID_FILE}" "${EC2_INSTANCE_USER}"@"${EC2_INSTANCE_HOST}":~/backups/"${FILE}" .
-
-RESULT_SCP=$?
-
-if [ $RESULT_SCP -eq 0 ]; then
-	tar -xf "${FILE}" ;
-else
-	echo "Doesn't exist: ${FILE}";
-	exit 1;
-fi
+scp -i "${ID_FILE}" "${EC2_INSTANCE_USER}"@"${EC2_INSTANCE_HOST}":~/backups/"${FILE}" . &&
+echo '================================ End download backup ==================================='
