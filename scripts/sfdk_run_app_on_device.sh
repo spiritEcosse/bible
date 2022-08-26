cfa959f17ed10d22fdc2c98a60a1138af8609cdb
spiritEcosse <shevchenkcoigor@gmail.com>
2022-08-26 12:48:05 +0200

    BCPA-72-clion

--- scripts/sfdk_run_app_on_device.sh
+++ scripts/sfdk_run_app_on_device.sh
@@ -5,7 +5,7 @@ source scripts/header.sh
 main() {
   prepare_aws_instance &&
   echo "-------------------------------- Ssh remote command --------------------------------- " &&
-  ssh -i "${ID_FILE}" "${DEPLOY_USER}"@"${EC2_INSTANCE_HOST}" "\
+  ssh -i "${ID_FILE}" "${EC2_INSTANCE_USER}"@"${EC2_INSTANCE_HOST}" "\
       $(typeset -f sfdk_run_app_on_device) &&
       sfdk_run_app_on_device
   "
