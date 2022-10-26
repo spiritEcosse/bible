67b3844b61d6f9b9a6ca73a17d8c520c0ab958da
spiritEcosse <shevchenkcoigor@gmail.com>
2022-10-26 10:02:09 +0200

    BCPA-44 BCPA BUG When we make second and subsequent updates 22h 28m

--- 3rdparty/llvm.cmake
+++ 3rdparty/llvm.cmake
@@ -2,12 +2,15 @@ message(STATUS "========================Start configure ${LIB_LLVM}=============
 
 set(LLVM_BUNDLE_DIR "${PROJECT_BINARY_DIR}/3rdparty/${LIB_LLVM}")
 set(LLVM_INSTALL_DIR "${LLVM_BUNDLE_DIR}/build")
-set(LLVM_SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/${LIB_LLVM}")
 
 if(NOT EXISTS "${LLVM_BUNDLE_DIR}")
 	execute_process(COMMAND bash -c "git clone --depth 1 https://github.com/llvm/llvm-project.git ${LIB_LLVM}"
 		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/3rdparty)
 endif()
+
+execute_process(COMMAND bash -c "ls -la ${LLVM_BUNDLE_DIR}"
+        RESULT_VARIABLE LS_LA_LLVM_INSTALL_DIR)
+message("LS_LA_LLVM_INSTALL_DIR: ${LS_LA_LLVM_INSTALL_DIR}")
 
 if(NOT EXISTS ${LLVM_INSTALL_DIR})
     execute_process(COMMAND bash -c "cmake -S llvm -B build -DLLVM_CODE_COVERAGE_TARGETS=llvm-cov -DCMAKE_BUILD_TYPE=Release -G '${CMAKE_GENERATOR}'"
