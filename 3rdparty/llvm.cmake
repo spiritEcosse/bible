940528673b7b76948b6e196f7bb69679ba38e55e
spiritEcosse <shevchenkcoigor@gmail.com>
2023-07-12 11:29:27 +0200

    trying to fix ninja not found; and throw error if llvm build is failed

--- 3rdparty/llvm.cmake
+++ 3rdparty/llvm.cmake
@@ -10,18 +10,21 @@ if(NOT EXISTS ${LLVM_INSTALL_DIR})
 		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/3rdparty)
 #endif()
 
-execute_process(COMMAND bash -c "\
-	cmake -S llvm \
-	-B build \
-	-G Ninja \
-	-DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;compiler-rt;llvm' \
-	-DLLVM_TARGETS_TO_BUILD='ARM;X86;AArch64' \
-	-DLLVM_ENABLE_ASSERTIONS=ON \
-	-DCMAKE_BUILD_TYPE=Release"
-        WORKING_DIRECTORY ${LLVM_BUNDLE_DIR} RESULT_VARIABLE OUTPUT_BUILD_LLVM)
-execute_process(COMMAND bash -c "ninja -C build check-all"
-        WORKING_DIRECTORY ${LLVM_BUNDLE_DIR})
-message("OUTPUT_BUILD_LLVM: ${OUTPUT_BUILD_LLVM}")
+	execute_process(COMMAND bash -c "\
+		cmake -S llvm \
+		-B build \
+		-DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;compiler-rt;llvm' \
+		-DLLVM_TARGETS_TO_BUILD='ARM;X86;AArch64' \
+		-DLLVM_ENABLE_ASSERTIONS=ON \
+		-DCMAKE_BUILD_TYPE=Release"
+			WORKING_DIRECTORY ${LLVM_BUNDLE_DIR} RESULT_VARIABLE OUTPUT_BUILD_LLVM)
+	execute_process(COMMAND bash -c "ninja -C build check-all"
+			WORKING_DIRECTORY ${LLVM_BUNDLE_DIR}
+			RESULT_VARIABLE LLVM_BUILD_RESULT)
+	message("OUTPUT_BUILD_LLVM: ${OUTPUT_BUILD_LLVM}")
+	if(NOT LLVM_BUILD_RESULT EQUAL "0")
+		message(FATAL_ERROR "cmake build llvm is failed")
+	endif()
 endif()
 
 set(ENV{PATH} "${LLVM_INSTALL_DIR}/bin:$ENV{PATH}")
