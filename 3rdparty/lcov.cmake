52e5924f2d320bea5171d2612df0b253fd0e8493
spiritEcosse <shevchenkcoigor@gmail.com>
2023-07-13 10:19:23 +0200

    add lcov to gitmodules

--- 3rdparty/lcov.cmake
+++ 3rdparty/lcov.cmake
@@ -3,11 +3,6 @@ message(STATUS "========================Start configure ${LIB_LCOV}=============
 set(LCOV_BUNDLE_DIR "${PROJECT_BINARY_DIR}/3rdparty/${LIB_LCOV}")
 set(LCOV_SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/${LIB_LCOV}")
 
-if(NOT EXISTS "${LCOV_SRC_DIR}")
-	execute_process(COMMAND bash -c "git clone --depth 1 https://github.com/linux-test-project/lcov.git ${LIB_LCOV}"
-		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty)
-endif()
-
 set(ENV{PATH} "${LCOV_SRC_DIR}/bin:$ENV{PATH}")
 
 message(STATUS "========================End configure ${LIB_LCOV}====================")
